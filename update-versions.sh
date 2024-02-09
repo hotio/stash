299a00fa6d8eaac08ff66aa314f6c6d6b598759b
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-09 09:20:42 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-set -e
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/releases/tags/latest_develop" | jq -re .target_commitish)
-[[ ${version} == "develop" ]] && version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/commits/develop" | jq -re .sha)
-curl -fsSL "https://github.com/stashapp/stash/releases/download/latest_develop/CHECKSUMS_SHA1" -o CHECKSUMS_SHA1
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/releases/tags/latest_develop" | jq -re .target_commitish) || exit 1
+[[ ${version} == "develop" ]] && version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/commits/develop" | jq -re .sha) || exit 1
+curl -fsSL "https://github.com/stashapp/stash/releases/download/latest_develop/CHECKSUMS_SHA1" -o CHECKSUMS_SHA1 || exit 1
 grep -q "${version:0:7}" < CHECKSUMS_SHA1
-curl -fsSL "https://github.com/stashapp/stash/releases/download/latest_develop/stash-linux" -o stash
+curl -fsSL "https://github.com/stashapp/stash/releases/download/latest_develop/stash-linux" -o stash || exit 1
 checksum="$(sha1sum stash | awk '{print $1}')"
 grep -q "${checksum}" < CHECKSUMS_SHA1
 rm -rf CHECKSUMS_SHA1 stash
