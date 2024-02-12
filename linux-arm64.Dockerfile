a07e501eba1871e4b77be8500650da327aea2b8d
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-12 06:08:08 +0100

    tweak version check [skip ci]

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -10,10 +10,10 @@ RUN apk add --no-cache ffmpeg python3 py3-requests sqlite-libs
 
 ARG VERSION
 RUN curl -fsSL "https://github.com/stashapp/stash/releases/download/latest_develop/CHECKSUMS_SHA1" > "${APP_DIR}/CHECKSUMS_SHA1" && \
-    cat "${APP_DIR}/CHECKSUMS_SHA1" | grep "${VERSION:0:7}" && \
+    grep "${VERSION:0:7}" < "${APP_DIR}/CHECKSUMS_SHA1" && \
     curl -fsSL "https://github.com/stashapp/stash/releases/download/latest_develop/stash-linux-arm64v8" > "${APP_DIR}/stash" && \
     CHECKSUM=$(sha1sum "${APP_DIR}/stash" | awk '{print $1}') && \
-    cat "${APP_DIR}/CHECKSUMS_SHA1" | grep "${CHECKSUM}" && \
+    grep "${CHECKSUM}" < "${APP_DIR}/CHECKSUMS_SHA1" && \
     chmod 755 "${APP_DIR}/stash"
 
 COPY root/ /
--- update-versions.sh
+++ update-versions.sh
@@ -2,10 +2,10 @@
 version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/releases/tags/latest_develop" | jq -re .target_commitish) || exit 1
 [[ ${version} == "develop" ]] && version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/commits/develop" | jq -re .sha) || exit 1
 curl -fsSL "https://github.com/stashapp/stash/releases/download/latest_develop/CHECKSUMS_SHA1" -o CHECKSUMS_SHA1 || exit 1
-grep -q "${version:0:7}" < CHECKSUMS_SHA1
+grep -q "${version:0:7}" < CHECKSUMS_SHA1 || exit 1
 curl -fsSL "https://github.com/stashapp/stash/releases/download/latest_develop/stash-linux" -o stash || exit 1
 checksum="$(sha1sum stash | awk '{print $1}')"
-grep -q "${checksum}" < CHECKSUMS_SHA1
+grep -q "${checksum}" < CHECKSUMS_SHA1 || exit 1
 rm -rf CHECKSUMS_SHA1 stash
 json=$(cat VERSION.json)
 jq --sort-keys \
