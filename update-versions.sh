6de9caf116c648bf7c4544229e7bc42810c999d5
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-12 06:29:27 +0100

    tweak update versions [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+old_version=$(jq -re '.version' < VERSION.json)
 version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/releases/latest" | jq -re .tag_name) || exit 1
-json=$(cat VERSION.json)
-jq --sort-keys \
-    --arg version "${version//v/}" \
-    '.version = $version' <<< "${json}" | tee VERSION.json
+if [[ "${version}" != "v${old_version}" ]]; then
+    curl -fsSL "https://github.com/stashapp/stash/releases/download/${version}/stash-linux" -o /dev/null || exit 1
+    json=$(cat VERSION.json)
+    jq --sort-keys \
+        --arg version "${version//v/}" \
+        '.version = $version' <<< "${json}" | tee VERSION.json
+fi
