254d44d4b5654df4663e4af4fdc9e8294132548b
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-04-26 07:13:19 +0200

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 old_version=$(jq -re '.version' < VERSION.json)
 version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/releases/latest" | jq -re .tag_name) || exit 1
+[[ -z ${version} ]] && exit 0
+[[ ${version} == null ]] && exit 0
 if [[ "${version}" != "v${old_version}" ]]; then
     curl -fsSL "https://github.com/stashapp/stash/releases/download/${version}/stash-linux" -o /dev/null || exit 1
     json=$(cat VERSION.json)
