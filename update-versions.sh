fb97bb69b74f2d9bf4219da7eef94fc79bafa5df
mrhotio <26902309+mrhotio@users.noreply.github.com>
2025-04-20 12:42:43 +0200

    exit 0

--- update-versions.sh
+++ update-versions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 old_version=$(jq -re '.version' < VERSION.json)
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/releases/latest" | jq -re .tag_name) || exit 1
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/stashapp/stash/releases/latest" | jq -re .tag_name) || exit 0
 [[ -z ${version} ]] && exit 0
 [[ ${version} == null ]] && exit 0
 if [[ "${version}" != "v${old_version}" ]]; then
     curl -fsSL "https://github.com/stashapp/stash/releases/download/${version}/stash-linux" -o /dev/null || exit 0
 fi
-intel_cr_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name') || exit 1
-intel_gc_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/intel-graphics-compiler/releases/latest" | jq -re '.tag_name') || exit 1
+intel_cr_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name') || exit 0
+intel_gc_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/intel/intel-graphics-compiler/releases/latest" | jq -re '.tag_name') || exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
