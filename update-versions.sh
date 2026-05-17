8750d965bb72e56336f3fb2cc8300c4abdf7f08a
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-05-17 14:38:44 +0200

    update header [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -2,14 +2,14 @@
 set -exuo pipefail
 
 old_version=$(jq -re '.version' < meta.json)
-version=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/stashapp/stash/releases/latest" | jq -re .tag_name)
+version=$(curl -fsSL ${GITHUB_TOKEN:+--header "Authorization: Bearer ${GITHUB_TOKEN}"} "https://api.github.com/repos/stashapp/stash/releases/latest" | jq -re .tag_name)
 version_check() {
     if [[ "${version}" != "${old_version}" ]]; then
-        curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://github.com/stashapp/stash/releases/download/${version}/stash-linux" -o /dev/null || return 1
+        curl -fsSL ${GITHUB_TOKEN:+--header "Authorization: Bearer ${GITHUB_TOKEN}"} "https://github.com/stashapp/stash/releases/download/${version}/stash-linux" -o /dev/null || return 1
     fi
 }
 version_check || version="${old_version}"
-version_intel_cr=$(curl -fsSL --header "Authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name')
+version_intel_cr=$(curl -fsSL ${GITHUB_TOKEN:+--header "Authorization: Bearer ${GITHUB_TOKEN}"} "https://api.github.com/repos/intel/compute-runtime/releases/latest" | jq -re '.tag_name')
 json=$(cat meta.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
